f26d4f9b16d3b2cbd6a2523f565c9a490fe56f43
Mnqobi Samuel Dube <96314691+MnqobiSD2018@users.noreply.github.com>
2025-08-25 15:12:23 +0200

    updated app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import LendingFining from "./pages/LendingFining";
 import Reports from "./pages/Reports";
 import Settings from "./pages/Settings"
 import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
 import Landing from "./pages/Landing"; 
 import './App.css';
 
@@ -19,12 +20,12 @@ function App() {
           
           {/* Protected Routes */}
           <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" />
-          <Route path="/dashboard/members"  />
-          <Route path="/dashboard/books"  />
-          <Route path="/dashboard/lending"  />
-          <Route path="/dashboard/reports"  />
-          <Route path="/dashboard/settings" />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard/members" element={<MemberManagement />} />
+          <Route path="/dashboard/books" element={<BookManagement />} />
+          <Route path="/dashboard/lending" element={<LendingFining />} />
+          <Route path="/dashboard/reports" element={<Reports />} />
+          <Route path="/dashboard/settings" element={<Settings />} />
           
 
           {/* Catch all incorrect routes or links (custom 404 page)*/}
